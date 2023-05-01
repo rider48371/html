2be307d384903bbb8fca0fa4f172dff31cc31116
Frederic Rider <rider48371@gmail.com>
2023-05-01 19:25:33 -0400

    updating schedule c

--- calculator/javascript/schedulec.js
+++ calculator/javascript/schedulec.js
@@ -1,10 +1,39 @@
-const el =document.getElementById('years');
-const one =document.getElementById('one');
+const twoYear = document.querySelector('.twoyear__form');
 
-el.addEventListener('change', function handleChange(event){
-    if (event.target.value === '1 Year') {
-        box.style.visibility = 'visible';
-    } else {
-        box.style.visibility = 'hidden';
+const twoYear__btn = document.querySelector('.twoyear__btn');
+
+ 
+
+twoYear__btn.addEventListener('click', () => {
+
+    const visibility = twoYear.getAttribute('data-visible')
+
+ 
+
+    if(visibility === "false") {
+
+        twoYear.setAttribute('data-visible', "true")
+
+    } else if (visibility === "true") {
+
+        twoYear.setAttribute('data-visible', "false")
+
     }
-    });
+
+});
+
+ 
+
+twoYear__btn.addEventListener("click", ()=> {
+
+    if(twoYear__btn.value === "1 Year"){
+
+        twoYear__btn.value = "2 Years";
+
+    }else{
+
+        twoYear__btn.value="1 Year";
+
+    }
+
+});
