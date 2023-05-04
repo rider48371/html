9f32dfc690f256e2bc121e9e158e5728ab192078
Frederic Rider <rider48371@gmail.com>
2023-05-04 16:47:21 -0400

    completed project

--- scroll/app.js
+++ scroll/app.js
@@ -2,6 +2,8 @@ const menu = document.querySelector('#mobile-menu');
 
 const menuLinks = document.querySelector('.navbar__menu');
 
+const navLogo = document.querySelector('#navbar__logo');
+
  
 
 // Display Mobile Menu
@@ -17,3 +19,91 @@ const mobileMenu = () => {
  
 
 menu.addEventListener("click", mobileMenu);
+
+// Show active menu when scrolling
+
+const highlightMenu = () => {
+
+    const elem = document.querySelector('.highlight');
+
+    const homeMenu = document.querySelector('#home-page');
+
+    const aboutMenu = document.querySelector('#about-page');
+
+    const servicesMenu = document.querySelector('#services-page');
+
+    let scrollPos = window.scrollY
+
+ 
+
+    // Adds the highlight class to menu items
+
+    if(window.innerWidth > 960 && scrollPos < 600) {
+
+        homeMenu.classList.add('highlight')
+
+        aboutMenu.classList.remove('highlight')
+
+        return
+
+    }
+
+    else if (window.innerWidth > 960 && scrollPos < 1500) {
+
+        aboutMenu.classList.add('highlight')
+
+        homeMenu.classList.remove('highlight')
+
+        servicesMenu.classList.remove('highlight')
+
+        return
+
+    }
+
+    else if (window.innerWidth > 960 && scrollPos < 2500) {
+
+        servicesMenu.classList.add('highlight')
+
+        aboutMenu.classList.remove('highlight')
+
+    }
+
+ 
+
+    if((elem && window.innerWidth < 960 && scrollPos < 600) || elem)  {
+
+        elem.classList.remove('highlight')
+
+    }
+
+}
+
+ 
+
+window.addEventListener("scroll", highlightMenu);
+
+window.addEventListener("click", highlightMenu);
+
+ 
+
+//Close mobile menu when clicking on a menu item
+
+const hideMobileMenu = () => {
+
+    const menuBars = document.querySelector('.is-active')
+
+    if(window.innerWidth <= 960 && menuBars) {
+
+        menu.classList.toggle('is-active')
+
+        menuLinks.classList.remove('active')
+
+    }
+
+}
+
+ 
+
+menuLinks.addEventListener("click", hideMobileMenu);
+
+navLogo.addEventListener("click", hideMobileMenu);
