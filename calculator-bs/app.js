f1897a69e511d8504a11c09458d7790ce58a9a3c
Frederic Rider <rider48371@gmail.com>
2023-05-08 18:05:28 -0400

    updated html css and js

---
+++ calculator-bs/app.js
@@ -0,0 +1,271 @@
+const fulltimebtn = document.querySelector("#fulltimebtn");
+
+const fulltimeform = document.getElementById("fulltimeform");
+
+const variablebtn = document.querySelector("#variablebtn");
+
+const variableform = document.getElementById("variableform");
+
+const schedulecbtn = document.querySelector("#schedulecbtn");
+
+const schedulecform = document.getElementById("schedulecform");
+
+const schedebtn = document.querySelector("#schedebtn");
+
+const schedeform = document.getElementById("schedeform");
+
+const scheduleebtn = document.querySelector("#scheduleebtn");
+
+const scheduleeform = document.getElementById("scheduleeform");
+
+const corpbtn = document.querySelector("#corpbtn");
+
+const corpform = document.getElementById("corpform");
+
+const rentalbtn = document.querySelector("#rentalbtn");
+
+const rentalform = document.getElementById("rentalform");
+
+ 
+
+fulltimebtn.addEventListener("click", () => {
+
+  fulltimeform.classList.remove("hide");
+
+  fulltimeform.classList.add("show");
+
+  variableform.classList.add("hide");
+
+  variableform.classList.remove("show");
+
+  schedulecform.classList.add("hide");
+
+  schedulecform.classList.remove("show");
+
+  schedeform.classList.add("hide");
+
+  schedeform.classList.remove("show");
+
+  scheduleeform.classList.add("hide");
+
+  scheduleeform.classList.remove("show");
+
+  corpform.classList.add("hide");
+
+  corpform.classList.remove("show");
+
+  rentalform.classList.add("hide");
+
+  rentalform.classList.remove("show");
+
+});
+
+variablebtn.addEventListener("click", () => {
+
+  fulltimeform.classList.add("hide");
+
+  fulltimeform.classList.remove("show");
+
+  variableform.classList.remove("hide");
+
+  variableform.classList.add("show");
+
+  schedulecform.classList.add("hide");
+
+  schedulecform.classList.remove("show");
+
+  schedeform.classList.add("hide");
+
+  schedeform.classList.remove("show");
+
+  scheduleeform.classList.add("hide");
+
+  scheduleeform.classList.remove("show");
+
+  corpform.classList.add("hide");
+
+  corpform.classList.remove("show");
+
+  rentalform.classList.add("hide");
+
+  rentalform.classList.remove("show");
+
+});
+
+schedulecbtn.addEventListener("click", () => {
+
+  fulltimeform.classList.add("hide");
+
+  fulltimeform.classList.remove("show");
+
+  variableform.classList.add("hide");
+
+  variableform.classList.remove("show");
+
+  schedulecform.classList.remove("hide");
+
+  schedulecform.classList.add("show");
+
+  schedeform.classList.add("hide");
+
+  schedeform.classList.remove("show");
+
+  scheduleeform.classList.add("hide");
+
+  scheduleeform.classList.remove("show");
+
+  corpform.classList.add("hide");
+
+  corpform.classList.remove("show");
+
+  rentalform.classList.add("hide");
+
+  rentalform.classList.remove("show");
+
+});
+
+schedebtn.addEventListener("click", () => {
+
+  fulltimeform.classList.add("hide");
+
+  fulltimeform.classList.remove("show");
+
+  variableform.classList.add("hide");
+
+  variableform.classList.remove("show");
+
+  schedulecform.classList.add("hide");
+
+  schedulecform.classList.remove("show");
+
+  schedeform.classList.remove("hide");
+
+  schedeform.classList.add("show");
+
+  scheduleeform.classList.add("hide");
+
+  scheduleeform.classList.remove("show");
+
+  corpform.classList.add("hide");
+
+  corpform.classList.remove("show");
+
+  rentalform.classList.add("hide");
+
+  rentalform.classList.remove("show");
+
+});
+
+scheduleebtn.addEventListener("click", () => {
+
+  fulltimeform.classList.add("hide");
+
+  fulltimeform.classList.remove("show");
+
+  variableform.classList.add("hide");
+
+  variableform.classList.remove("show");
+
+  schedulecform.classList.add("hide");
+
+  schedulecform.classList.remove("show");
+
+  schedeform.classList.add("hide");
+
+  schedeform.classList.remove("show");
+
+  scheduleeform.classList.remove("hide");
+
+  scheduleeform.classList.add("show");
+
+  corpform.classList.add("hide");
+
+  corpform.classList.remove("show");
+
+  rentalform.classList.add("hide");
+
+  rentalform.classList.remove("show");
+
+});
+
+corpbtn.addEventListener("click", () => {
+
+  fulltimeform.classList.add("hide");
+
+  fulltimeform.classList.remove("show");
+
+  variableform.classList.add("hide");
+
+  variableform.classList.remove("show");
+
+  schedulecform.classList.add("hide");
+
+  schedulecform.classList.remove("show");
+
+  schedeform.classList.add("hide");
+
+  schedeform.classList.remove("show");
+
+  scheduleeform.classList.add("hide");
+
+  scheduleeform.classList.remove("show");
+
+  corpform.classList.remove("hide");
+
+  corpform.classList.add("show");
+
+  rentalform.classList.add("hide");
+
+  rentalform.classList.remove("show");
+
+});
+
+rentalbtn.addEventListener("click", () => {
+
+  fulltimeform.classList.add("hide");
+
+  fulltimeform.classList.remove("show");
+
+  variableform.classList.add("hide");
+
+  variableform.classList.remove("show");
+
+  schedulecform.classList.add("hide");
+
+  schedulecform.classList.remove("show");
+
+  schedeform.classList.add("hide");
+
+  schedeform.classList.remove("show");
+
+  scheduleeform.classList.add("hide");
+
+  scheduleeform.classList.remove("show");
+
+  corpform.classList.add("hide");
+
+  corpform.classList.remove("show");
+
+  rentalform.classList.remove("hide");
+
+  rentalform.classList.add("show");
+
+});
+
+ 
+
+const twoYear = document.querySelector(".twoyear__form");
+
+const twoYear__btn = document.querySelector(".twoyear__btn");
+
+ 
+
+twoYear__btn.addEventListener("click", () => {
+
+  const visibility = twoYear.getAttribute("data-visible");
+
+ 
+
+  if (visibility === "false") {
+
+    twoYear.setAttribute
