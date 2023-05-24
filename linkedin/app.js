e489b5e39164ce003cd08fdfddd281e3306e6458
Frederic Rider <rider48371@gmail.com>
2023-05-24 17:06:38 -0400

    adding js and profile page

---
+++ linkedin/app.js
@@ -0,0 +1,18 @@
+let profileMenu = document.getElementById("profileMenu");
+
+let seeMore = document.getElementById("see-more");
+
+ 
+
+function toggleMenu() {
+
+  profileMenu.classList.toggle("open-menu");
+
+}
+
+function toggleMenu1() {
+
+  seeMore.classList.toggle("hidden");
+
+}
+
