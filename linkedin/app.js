6eb49ca52a4d91fca3ad4248bd4c388dcd7f8003
Frederic Rider <rider48371@gmail.com>
2023-05-26 17:56:30 -0400

    finished project

--- linkedin/app.js
+++ linkedin/app.js
@@ -2,6 +2,12 @@ let profileMenu = document.getElementById("profileMenu");
 
 let seeMore = document.getElementById("see-more");
 
+let seeLess = document.getElementById("see-more-link");
+
+let sidebarActivity = document.getElementById("sidebarActivity");
+
+let moreLink = document.getElementById("showMoreLink");
+
  
 
 function toggleMenu() {
@@ -14,5 +20,30 @@ function toggleMenu1() {
 
   seeMore.classList.toggle("hidden");
 
+  if (seeMore.classList.contains("hidden")) {
+
+    seeLess.innerHTML = "See More...";
+
+  } else {
+
+    seeLess.innerHTML = "See Less...";
+
+  }
+
 }
 
+function toggleActivity() {
+
+  sidebarActivity.classList.toggle("open-activity");
+
+  if (sidebarActivity.classList.contains("open-activity")) {
+
+    moreLink.innerHTML = "Show Less <b>-</b>";
+
+  } else {
+
+    moreLink.innerHTML = "Show more <b>+</b>";
+
+  }
+
+}
