76f812a8cb27f928b6f55784e2a3a7f949557930
Frederic Rider <rider48371@gmail.com>
2023-06-05 17:48:43 -0400

    finished project

---
+++ basic-calc/app.js
@@ -0,0 +1,67 @@
+const buttons = document.querySelectorAll("button");
+
+const inputField = document.getElementById("result");
+
+ 
+
+for (let i = 0; i < buttons.length; i++) {
+
+  buttons[i].addEventListener("click", () => {
+
+    const buttonValue = buttons[i].textContent;
+
+    console.log(buttonValue);
+
+    if (buttonValue === "CE") {
+
+      clearResult();
+
+    } else if (buttonValue === "=") {
+
+      calculateResult();
+
+    } else if (buttonValue === "del") {
+
+      delDigit(buttonValue);
+
+    } else {
+
+      appendValue(buttonValue);
+
+    }
+
+  });
+
+}
+
+ 
+
+function clearResult() {
+
+  inputField.value = "";
+
+}
+
+ 
+
+function calculateResult() {
+
+  inputField.value = eval(inputField.value);
+
+}
+
+ 
+
+function appendValue(buttonValue) {
+
+  inputField.value += buttonValue;
+
+}
+
+ 
+
+function delDigit(buttonValue) {
+
+  inputField.value = inputField.value.substring(0, inputField.value.length - 1);
+
+}
