1fd8b3484022729d07c6ed8d34bc0c1e0a520554
Frederic Rider <rider48371@gmail.com>
2023-06-13 18:59:42 -0400

    finished fulltime income calculations

--- calculator-bs/app.js
+++ calculator-bs/app.js
@@ -26,6 +26,14 @@ const rentalbtn = document.querySelector("#rentalbtn");
 
 const rentalform = document.getElementById("rentalform");
 
+const schedcBtn = document.getElementById("twoyear__btn");
+
+const schedeBtn = document.getElementById("schedetwoyear__btn");
+
+const scheduleeBtn = document.getElementById("scheduleetwoyear__btn");
+
+const corpBtn = document.getElementById("corptwoyear__btn");
+
  
 
 fulltimebtn.addEventListener("click", () => {
@@ -60,6 +68,8 @@ fulltimebtn.addEventListener("click", () => {
 
 });
 
+ 
+
 variablebtn.addEventListener("click", () => {
 
   fulltimeform.classList.add("hide");
@@ -91,6 +101,8 @@ variablebtn.addEventListener("click", () => {
   rentalform.classList.remove("show");
 
 });
+
+ 
 
 schedulecbtn.addEventListener("click", () => {
 
@@ -124,6 +136,8 @@ schedulecbtn.addEventListener("click", () => {
 
 });
 
+ 
+
 schedebtn.addEventListener("click", () => {
 
   fulltimeform.classList.add("hide");
@@ -155,6 +169,8 @@ schedebtn.addEventListener("click", () => {
   rentalform.classList.remove("show");
 
 });
+
+ 
 
 scheduleebtn.addEventListener("click", () => {
 
@@ -188,6 +204,8 @@ scheduleebtn.addEventListener("click", () => {
 
 });
 
+ 
+
 corpbtn.addEventListener("click", () => {
 
   fulltimeform.classList.add("hide");
@@ -219,6 +237,8 @@ corpbtn.addEventListener("click", () => {
   rentalform.classList.remove("show");
 
 });
+
+ 
 
 rentalbtn.addEventListener("click", () => {
 
@@ -254,79 +274,263 @@ rentalbtn.addEventListener("click", () => {
 
  
 
-const twoYear = document.querySelector(".twoyear__form");
+// Hiding 2nd year forms for 1 year calculation
 
-const twoYear__btn = document.querySelector(".twoyear__btn");
+function schedcBtnClick() {
 
- 
-
-twoYear__btn.addEventListener("click", () => {
-
-  const visibility = twoYear.getAttribute("data-visible");
+  const schedcForm = document.getElementById("twoyear__form");
 
  
 
-  if (visibility === "false") {
+  if (schedcBtn.value === "1 Year") {
 
-    twoYear.setAttribute("data-visible", "false");
+    schedcBtn.value = "2 Years";
+
+  } else if (schedcBtn.value === "2 Years") {
+
+    schedcBtn.value = "1 Year";
 
   }
 
-});
+  schedcForm.classList.toggle("hide");
+
+}
+
+function schedeBtnClick() {
+
+  const schedeForm = document.getElementById("schedetwoyear__form");
 
  
 
-scheduleeTwoYear__btn.addEventListener("click", () => {
+  if (schedeBtn.value === "1 Year") {
 
-  if (scheduleeTwoYear__btn.value === "1 Year") {
+    schedeBtn.value = "2 Years";
 
-    schedeTwoYear__btn.value = "2 Years";
+  } else if (schedeBtn.value === "2 Years") {
 
-  } else {
-
-    scheduleeTwoYear__btn.value = "1 Year";
+    schedeBtn.value = "1 Year";
 
   }
 
-});
+  schedeForm.classList.toggle("hide");
+
+}
+
+function scheduleeBtnClick() {
+
+  const scheduleeForm = document.getElementById("scheduleetwoyear__form");
 
  
 
-const corpTwoYear = document.querySelector(".corptwoyear__form");
+  if (scheduleeBtn.value === "1 Year") {
 
-const corpTwoYear__btn = document.querySelector(".corptwoyear__btn");
+    scheduleeBtn.value = "2 Years";
 
- 
+  } else if (scheduleeBtn.value === "2 Years") {
 
-corpTwoYear__btn.addEventListener("click", () => {
-
-  const visibility = corpTwoYear.getAttribute("data-visible");
-
- 
-
-  if (visibility === "false") {
-
-    corpTwoYear.setAttribute("data-visible", "true");
-
-  } else if (visibility === "true") {
-
-    corpTwoYear.setAttribute("data-visible", "false");
+    scheduleeBtn.value = "1 Year";
 
   }
 
-});
+  scheduleeForm.classList.toggle("hide");
+
+}
+
+function corpBtnClick() {
+
+  const corpForm = document.getElementById("corptwoyear__form");
 
  
 
-corpTwoYear__btn.addEventListener("click", () => {
+  if (corpBtn.value === "1 Year") {
 
-  if (corpTwoYear__btn.value === "1 Year") {
+    corpBtn.value = "2 Years";
 
-    corpTwoYear__btn.value = "2 Years";
+  } else if (corpBtn.value === "2 Years") {
 
-  } else {
+    corpBtn.value = "1 Year";
 
-    corpTwoYear__btn.value = "1 Year";
+  }
+
+  corpForm.classList.toggle("hide");
+
+}
+
+ 
+
+// Math Section
+
+ 
+
+// Fulltime
+
+const monthEl = document.getElementById("month");
+
+const dayEl = document.getElementById("day");
+
+const yearEl = document.getElementById("year");
+
+const btnEl = document.getElementById("ftbtn");
+
+const payEl = document.getElementById("pay");
+
+const paytypeEl = document.getElementById("paytype");
+
+const ytdEl = document.getElementById("ytd");
+
+const fullResult = document.getElementById("fullresult");
+
+ 
+
+btnEl.addEventListener("click", () => {
+
+  let lastYear = lastyear.value / 12;
+
+  var getDaysInMonth = function(month, year) {
+
+    return new Date(year, month, 0).getDate();
+
+  };
+
+  let timeElapsed = (
+
+    monthEl.value -
+
+    Number(1) +
+
+    dayEl.value / getDaysInMonth(monthEl.value, yearEl.value)
+
+  ).toFixed(5);
+
+ 
+
+  if (paytypeEl.value === "hourly") {
+
+    let fullIncome = ((payEl.value * 40 * 52) / 12).toFixed(2);
+
+    let ytdIncome = (ytdEl.value / timeElapsed).toFixed(2);
+
+    if (ytdIncome > fullIncome * Number(0.9)) {
+
+      fullResult.innerText = fullIncome + "/month";
+
+    } else {
+
+      fullResult.innerHTML = ytdIncome + "/month. <b>May be variable.</b>";
+
+    }
+
+  } else if (paytypeEl.value === "weekly") {
+
+    let weeklyIncome = ((payEl.value * 52) / 12).toFixed(2);
+
+    let ytdIncome = (ytdEl.value / timeElapsed).toFixed(2);
+
+    if (ytdIncome > weeklyIncome * Number(0.9)) {
+
+      fullResult.innerText = weeklyIncome + "/month";
+
+    } else if (ytdIncome < weeklyIncome * Number(0.9)) {
+
+      fullResult.innerHTML = ytdIncome + "/month. <b>May be variable.</b>";
+
+    }
+
+  } else if (paytypeEl.value === "biweekly") {
+
+    let biweeklyIncome = ((payEl.value * 26) / 12).toFixed(2);
+
+    let ytdIncome = (ytdEl.value / timeElapsed).toFixed(2);
+
+    if (ytdIncome > biweeklyIncome * Number(0.9)) {
+
+      fullResult.innerText = biweeklyIncome + "/month";
+
+    } else if (ytdIncome < biweeklyIncome * Number(0.9)) {
+
+      fullResult.innerHTML = ytdIncome + "/month. <b>May be variable.</b>";
+
+    }
+
+  } else if (paytypeEl.value === "semimonthly") {
+
+    let semimonthlyIncome = ((payEl.value * 24) / 12).toFixed(2);
+
+    let ytdIncome = (ytdEl.value / timeElapsed).toFixed(2);
+
+    if (ytdIncome > semimonthlyIncome * Number(0.9)) {
+
+      fullResult.innerText = semimonthlyIncome + "/month";
+
+    } else if (ytdIncome < semimonthlyIncome * Number(0.9)) {
+
+      fullResult.innerHTML = ytdIncome + "/month. <b>May be variable.</b>";
+
+    }
+
+  } else if (paytypeEl.value === "monthly") {
+
+    let monthlyIncome = payEl.value;
+
+    let ytdIncome = (ytdEl.value / timeElapsed).toFixed(2);
+
+    if (ytdIncome > monthlyIncome * Number(0.9)) {
+
+      fullResult.innerText = monthlyIncome + "/month";
+
+    } else if (ytdIncome < monthlyIncome * Number(0.9)) {
+
+      fullResult.innerHTML = ytdIncome + "/month. <b>May be variable.</b>";
+
+    }
+
+  } else if (paytypeEl.value === "annual") {
+
+    let annualIncome = (payEl.value / 12).toFixed(2);
+
+    let ytdIncome = (ytdEl.value / timeElapsed).toFixed(2);
+
+    if (ytdIncome > annualIncome * Number(0.9)) {
+
+      fullResult.innerText = annualIncome + "/month";
+
+    } else if (ytdIncome < annualIncome * Number(0.9)) {
+
+      fullResult.innerHTML = ytdIncome + "/month. <b>May be variable.</b>";
+
+    }
+
+  } else if (paytypeEl.value === "monthlyteach") {
+
+    let monthlyTeachIncome = ((payEl.value * 10) / 12).toFixed(2);
+
+    let ytdIncome = (ytdEl.value / timeElapsed).toFixed(2);
+
+    if (ytdIncome > monthlyTeachIncome * Number(0.9)) {
+
+      fullResult.innerText = monthlyTeachIncome + "/month";
+
+    } else if (ytdIncome < monthlyTeachIncome * Number(0.9)) {
+
+      fullResult.innerHTML = ytdIncome + "/month. <b>May be variable.</b>";
+
+    }
+
+  } else if (paytypeEl.value === "annualteach") {
+
+    let annualTeachIncome = (payEl.value / 12).toFixed(2);
+
+    let ytdIncome = (ytdEl.value / timeElapsed).toFixed(2);
+
+    if (ytdIncome > annualTeachIncome * Number(0.9)) {
+
+      fullResult.innerText = annualTeachIncome + "/month";
+
+    } else if (ytdIncome < annualTeachIncome * Number(0.9)) {
+
+      fullResult.innerHTML = ytdIncome + "/month. <b>May be variable.</b>";
+
+    }
 
   }
 
