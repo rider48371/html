373246001ff2d75d4f88debb177c881359a5df2e
Fred Rider <rider48371@gmail.com>
2024-06-05 12:25:06 -0400

    updating app.js

--- React Practice/calculator/src/App.js
+++ React Practice/calculator/src/App.js
@@ -1,7 +1,8 @@
 import{ useReducer } from "react"
 import "./style.css"
+import DigitButton from "./DigitButton"
 
-const ACTIONS = {
+export const ACTIONS = {
   ADD_DIGIT: 'add-digit',
   CHOOSE_OPERATION: 'choose-operation',
   CLEAR: 'clear',
@@ -12,10 +13,10 @@ const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
-      return (
+      return {
         ...state,
-        currentOperand: '${currentOperand || ""}${payload.digit}',
-      )
+        currentOperand: '${state.currentOperand || ""}${payload.digit}',
+      }
   }
 }
 
@@ -29,7 +30,7 @@ function App() {
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
-      <button>/</button>
+      <DigitButton digit="/" dispatch={dispatch} />
       <button>1</button>
       <button>2</button>
       <button>3</button>
