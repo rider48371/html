1dd34eeea289e87ad784a88c50ae3ac3b18f5f02
Fred Rider <rider48371@gmail.com>
2025-10-26 00:37:17 -0400

    completed movies app

---
+++ react/movies/frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import './css/App.css'
+import Favorites from "./pages/Favorites"
+import Navbar from "./components/Navbar"
+import {Route, Routes} from "react-router-dom"
+import Home from "./pages/Home"
+import {MovieProvider} from "./contexts/MovieContext"
+
+function App() {
+  return (
+    <MovieProvider>
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/favorites" element={<Favorites />}/>
+        </Routes>
+      </main>
+    </MovieProvider>
+  )
+}
+
+export default App
